7f7acc22ae8cebf9b55b62ab87c98f84ce525a97
Felipe Chandía Castillo <fchandiac@gmail.com>
2024-06-18 14:16:29 -0400

    180624-10

--- components/forms/CustomerForm.js
+++ components/forms/CustomerForm.js
@@ -15,7 +15,7 @@ import useUtils from "../hooks/useUtils";
 import SearchIcon from "@mui/icons-material/Search";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import useRecords from "../hooks/useRecords";
-const useCustomers = require("../hooks/useCustomers").default;
+import useCustomers from "@/components/hooks/useCustomers";
 
 
 
